8666c20fb3d1f8bc2dc46f29afaa233005964112
Richard <richard_h199@hotmail.com>
2016-08-16 07:01:35 +1000

    final commit

--- main.ino/main.ino.ino
+++ main.ino/main.ino.ino
@@ -18,61 +18,50 @@
 
 //LINE FOLLOWERS
 
-int trig = 52; // attach pin 3 to Trig
-int echo = 53; //attach pin 4 to Echo
-void setup() {
+//int trig = 50; // attach pin 3 to Trig
+//int echo = 51; //attach pin 4 to Echo
+
+#include "RB_Movement.h"
+#include "RB_Motor.h"
+
+
+int btnPin = 24;
+bool pressed = 0;
+bool state = 0;
+
+Movement bot(13, 12, 11, 10, 9, 8); //this creates an instance of the object and sets motor pins
+
+void setup() { //nothing is needed in setup
+  pinMode(btnPin, INPUT_PULLUP);
   Serial.begin(9600);
-
 }
 
-void loop()
-{
-// establish variables for duration of the ping,
-// and the distance result in inches and centimeters:
-long duration, inches, cm;
+void loop() {
+  pressed = !digitalRead(btnPin);
+  if (pressed == 0) {
+    bot.allStop(); //stop the robot
+  }
+  else {
+    state = !state;
+    Serial.println("yes");
+    delay(100);
+  }
 
-// The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-pinMode(trig, OUTPUT);
-digitalWrite(trig, LOW);
-delayMicroseconds(2);
-digitalWrite(trig, HIGH);
-delayMicroseconds(5);
-digitalWrite(trig, LOW);
+  if (state == 1) {
+    bot.manualMotors(1,50,10); //drive the motors with comma seperated values
+    delay(100);
+    Serial.println("on!");
+  }
+  else {
 
-// The same pin is used to read the signal from the PING))): a HIGH
-// pulse whose duration is the time (in microseconds) from the sending
-// of the ping to the reception of its echo off of an object.
-pinMode(echo,INPUT);
-duration = pulseIn(echo, HIGH);
-
-// convert the time into a distance
-inches = microsecondsToInches(duration);
-cm = microsecondsToCentimeters(duration);
-
-Serial.print(inches);
-Serial.print("in, ");
-Serial.print(cm);
-Serial.print("cm");
-Serial.println();
-
-delay(100);
-}
-
-long microsecondsToInches(long microseconds)
-{
-// According to Parallax's datasheet for the PING))), there are
-// 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-// second). This gives the distance travelled by the ping, outbound
-// and return, so we divide by 2 to get the distance of the obstacle.
-// See: http://www.parallax.com/dl/docs/prod/acc/28015-PI...
-return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds)
-{
-// The speed of sound is 340 m/s or 29 microseconds per centimeter.
-// The ping travels out and back, so to find the distance of the
-// object we take half of the distance travelled.
-return microseconds / 29 / 2;
+  }
+  Serial.println(pressed);
+  
+  // delay(1000);
+  // bot.allStop(); //stop the robot
+  // delay(1000);
+  // bot.moveStraight(180,100); //move backwards at full speed
+  // delay(1000);
+  // bot.moveStraight(0,0); //another way of stopping the robot
+  // delay(1000);
 }
